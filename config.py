1010a0e42eb9b7eb8991b225183b7befd8b54578
Ronnie Beltran <rbbeltran.09@gmail.com>
2014-08-12 14:45:51 +0800

    Put client id and secret

---
+++ config.py
@@ -0,0 +1,3 @@
+APPSPOT_HOST = 'http://devcup-2014.appspot.com/'
+WEB_CLIENT_ID = '895496255134-nidptltk7anp5o8f9m1c11251cmaocjn.apps.googleusercontent.com'
+WEB_CLIENT_SECRET = '2osAMwzQ2ApbDDr4dVvgrGg_'
