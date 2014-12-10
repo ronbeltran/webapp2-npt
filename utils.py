d6f3c8d01ff65e273e371f03c41ae27b9ea2ef6b
Ronnie Beltran <rbbeltran.09@gmail.com>
2014-12-10 15:48:40 +0800

    add docstring for jinja2_factory

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from webapp2_extras import jinja2
 
 
 def jinja2_factory(app):
+    ''' Use `uri_for` tag in jinja2 templates
+    Example usage: {{ uri_for('home') }}
+    '''
     jinja_app = jinja2.Jinja2(app)
     jinja_app.environment.globals.update({
         'uri_for': webapp2.uri_for,
